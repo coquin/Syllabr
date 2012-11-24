b475dbd1fb5dc9eb7849143bdbd7f6c400b449af
Denis Kokin <denis.kokin@gmail.com>
2012-11-24 21:51:53 +0400

    Реализован подсчет слогов

--- Form1.cs
+++ Form1.cs
@@ -23,12 +23,47 @@ namespace Syllabr
         {
             if (txt != null && txt.Length > 0)
             {
-                clipboardLabel.Text = detectLanguage(txt);
+                string lng = detectLanguage(txt);
+
+                if (lng == "RUS")
+                {
+                    clipboardLabel.Text = countRus(txt).ToString();
+                }
+                else
+                {
+                    clipboardLabel.Text = countEng(txt).ToString();
+                }
             }
             else
             {
                 clipboardLabel.Text = "No text captured";
             }
+        }
+
+        // Вычисляет число слогов в русском тексте
+        // Просто считает число гласных
+        private int countRus(string txt)
+        {
+            Regex rgx = new Regex(@"[аеёиоуыэюя]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            MatchCollection matches = rgx.Matches(txt);
+            return matches.Count;
+        }
+
+        // Вычисляет число слогов в английском тексте
+        private int countEng(string txt)
+        {
+            Regex replaceRgx = new Regex(@"\by", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            Regex syllabesRgx = new Regex(@"cion|tion|sion|eau|iou|ae|ai|ay|au|ee|ea|ei|eu|ey|ie|oa|oe|oi|oo|ou|oy|ue|ui|[aueioy]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            Regex eAtEndRgx = new Regex(@"e\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            Regex wordsRgx = new Regex(@"\b[wbmhnrs]e\b|\bthe\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            txt = replaceRgx.Replace(txt, "");
+
+            MatchCollection syllabesMatches = syllabesRgx.Matches(txt);
+            MatchCollection eAtEndMatches = eAtEndRgx.Matches(txt);
+            MatchCollection wordsMatches = wordsRgx.Matches(txt);
+
+            return syllabesMatches.Count - eAtEndMatches.Count + wordsMatches.Count;
         }
 
         // Определяет язык текстового фрагмента
