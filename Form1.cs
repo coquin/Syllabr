52f2dddaccea09acc09bac457452a857b26dd942
Denis Kokin <denis.kokin@gmail.com>
2012-11-24 21:33:38 +0400

    Простое определение языка
    Научился детектить язык текстового фрагмента в буфере обмена (русский
    или не русский)

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,14 +16,14 @@ namespace Syllabr
         public Form1()
         {
             InitializeComponent();
-            setLabelText(Clipboard.GetText());
+            setLabelText(Clipboard.GetText(TextDataFormat.UnicodeText));
         }
 
         private void setLabelText(string txt="")
         {
             if (txt != null && txt.Length > 0)
             {
-                clipboardLabel.Text = txt;
+                clipboardLabel.Text = detectLanguage(txt);
             }
             else
             {
@@ -30,9 +31,26 @@ namespace Syllabr
             }
         }
 
+        // Определяет язык текстового фрагмента
+        // Пока по тупому ищет гласные русского языка, если находит, то считает,
+        // что имеет дело с русским языком, в противном случае - с английским
+        private string detectLanguage(string txt)
+        {
+            Regex rgx = new Regex(@"[аеёиоуыэюя]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            if (rgx.IsMatch(txt))
+            {
+                return "RUS";
+            }
+            else
+            {
+                return "ENG";
+            }
+        }
+
         private void clipboardMonitor1_ClipboardChanged(object sender, ClipboardChangedEventArgs e)
         {
-            setLabelText((string)e.DataObject.GetData(DataFormats.Text));
+            setLabelText((string)e.DataObject.GetData(DataFormats.UnicodeText));
         }
     }
 }
