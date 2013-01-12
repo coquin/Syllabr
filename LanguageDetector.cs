ec3ccaf836d6ad6376df50d1807cbb6dc1e41471
Denis Kokin <denis.kokin@gmail.com>
2013-01-12 12:21:45 +0400

    Рефакторинг функционала определения языка.
    Вынес определение языка в отдельный класс LanguageDetector.

---
+++ LanguageDetector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Syllabr
+{
+    class LanguageDetector
+    {
+        public const string ENG = "ENG";
+        public const string RUS = "RUS";
+
+        private Regex languageDetectionRegex = new Regex(@"[аеёиоуыэюя]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        // Определяет язык текстового фрагмента
+        // Пока по тупому ищет гласные русского языка, если находит, то считает,
+        // что имеет дело с русским языком, в противном случае - с английским
+        public string detectLanguage(string text)
+        {
+            if (languageDetectionRegex.IsMatch(text))
+            {
+                return RUS;
+            }
+            else
+            {
+                return ENG;
+            }
+        }
+    }
+}
--- MainForm.cs
+++ MainForm.cs
@@ -5,7 +5,6 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -21,8 +20,9 @@ namespace Syllabr
 
         private SyllableCounterRUS counterRUS = new SyllableCounterRUS();
         private SyllableCounterENG counterENG = new SyllableCounterENG();
-
         private ISyllableCounter counter;
+
+        private LanguageDetector languageDetector = new LanguageDetector();
 
         private void setLabelText(string txt="")
         {
@@ -32,9 +32,9 @@ namespace Syllabr
             {
                 if (txt.Length > 0)
                 {
-                    lng = detectLanguage(txt);
+                    lng = languageDetector.detectLanguage(txt);
 
-                    if (lng == "RUS")
+                    if (lng == LanguageDetector.RUS)
                     {
                         counter = counterRUS;
                     }
@@ -53,23 +53,6 @@ namespace Syllabr
             catch (Exception)
             {
                 clipboardLabel.Text = "No text captured!";
-            }
-        }
-
-        // Определяет язык текстового фрагмента
-        // Пока по тупому ищет гласные русского языка, если находит, то считает,
-        // что имеет дело с русским языком, в противном случае - с английским
-        private string detectLanguage(string txt)
-        {
-            Regex rgx = new Regex(@"[аеёиоуыэюя]", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-
-            if (rgx.IsMatch(txt))
-            {
-                return "RUS";
-            }
-            else
-            {
-                return "ENG";
             }
         }
 
